67d8ca1a63dd726257292c3a60fb17cd9e7694bc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-15 02:58:24 +0900

    toolbarDefinition.sh

--- a-shell/toolbarDefinition.sh
+++ a-shell/toolbarDefinition.sh
@@ -1,52 +1,33 @@
 # Button icon				action				parameter
-escape  insertString  \u{001B}
-chevron.up.square  systemAction  control
 arrow.right.to.line.alt  insertString  \u{0009}
-doc.on.clipboard  systemAction  paste
+chevron.up.square  systemAction  control
+
 
 [="none"
   arrow.up  systemAction  up
   arrow.down  systemAction  down
   arrow.left  systemAction  left
   arrow.right  systemAction  right
-]
-
-[="vim"
-  escape  insertString  \u{001B}
-  chevron.up.square  systemAction  control
-  arrow.right.to.line.alt  insertString  \u{0009}
   doc.on.clipboard  systemAction  paste
 ]
 
+separator
+
+.  insertString  .
+$  insertString  $
+-  insertString  -
+*  insertString  *
+/  insertString  /
 
 
-# Example buttons:
-#
-# delete.backward           insertString        \u{007F}
-# return                    insertString        \u{000D}
-# switch.2					insertString		vim .toolbarDefinition\n
-# calendar.badge.clock		insertCommand		date "+%Y_%m_%d"
+[="vim"
+  +  insertString  +
+  %  insertString  %
+  "  insertString  "
+  (  insertString  (
+  [  insertString  [
+  :  insertString  :
+]
 
-# Example groups (only with iPads and iOS-style toolbar). Max 15 commands in a submenu
-# [
-#     scissors						systemAction		cut
-#     arrow.up.doc.on.clipboard		systemAction		copy
-#     doc.on.clipboard				systemAction		paste
-# ] filemenu.and.cursorarrow
-
-# This one is shown only if no commands are running:
-# [="none"
-#     ls				insertString		ls -a ~/Documents/
-#     uname				insertString		uname -a
-#     ping 🍎			insertString		ping www.apple.com
-#     date				insertString		date
-# ]
-
-# This one appears if you edit a Markdown file in Vim:
-# [="vim .*\.md"
-#     key					insertString		\u{001B}:q!\n
-#     bold				insertString		:s/\\%V.*\\%V./**&**\n
-#     italic				insertString		:s/\\%V.*\\%V./*&*\n
-#     strikethrough		insertString		:s/\\%V.*\\%V./\\~\\~&\\~\\~\n
-# ] contextualmenu.and.cursorarrow
+escape  insertString  \u{001B}
 
